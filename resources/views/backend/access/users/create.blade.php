dca9c286aa6df25d1f0ecd49073c774ef4e71fb6
NGUYEN NGOC TUAN <VACNGTU001@VIETACORP.NET>
2020-08-18 14:22:12 +0700

    reformat form create user

--- resources/views/backend/access/users/create.blade.php
+++ resources/views/backend/access/users/create.blade.php
@@ -76,16 +76,16 @@
             </div><!--form control-->
             {{-- Phòng --}}
             <div class="form-group">
-        {{ Form::label('categories', trans('validation.attributes.backend.users.category'), ['class' => 'col-lg-2 control-label required']) }}
+                {{ Form::label('categories', trans('validation.attributes.backend.users.category'), ['class' => 'col-lg-2 control-label required']) }}
 
-        <div class="col-lg-10">
-        @if(!empty($selectedCategories))
-            {{ Form::select('categories[]', $blogCategories, $selectedCategories, ['class' => 'form-control tags box-size', 'required' => 'required', 'multiple' => 'multiple']) }}
-        @else
-            {{ Form::select('categories[]', $blogCategories, null, ['class' => 'form-control tags box-size', 'required' => 'required', 'multiple' => 'multiple']) }}
-        @endif
-        </div><!--col-lg-10-->
-    </div><!--form control-->
+                <div class="col-lg-10">
+                    @if(!empty($selectedCategories))
+                        {{ Form::select('categories[]', $blogCategories, $selectedCategories, ['class' => 'form-control tags box-size', 'required' => 'required', 'multiple' => 'multiple']) }}
+                    @else
+                        {{ Form::select('categories[]', $blogCategories, null, ['class' => 'form-control tags box-size', 'required' => 'required', 'multiple' => 'multiple']) }}
+                    @endif
+                </div><!--col-lg-10-->
+            </div><!--form control-->
 
 
             {{-- Status --}}
