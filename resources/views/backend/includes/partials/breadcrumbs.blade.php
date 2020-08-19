3f3ba46c56a6fca693ae9cbea941e9bf7dd62ce8
NGUYEN NGOC TUAN <VACNGTU001@VIETACORP.NET>
2020-08-19 16:16:32 +0700

    fix breadcrumbs

--- resources/views/backend/includes/partials/breadcrumbs.blade.php
+++ resources/views/backend/includes/partials/breadcrumbs.blade.php
@@ -1,11 +1,17 @@
 @if ($breadcrumbs)
-    <ol class="breadcrumb">
-        @foreach ($breadcrumbs as $breadcrumb)
-            @if ($breadcrumb->url && !$loop->last)
-                <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-            @else
-                <li class="active">{{ $breadcrumb->title }}</li>
-            @endif
-        @endforeach
-    </ol>
+    <div class="row page-titles">
+        <div class="col-md-12 align-self-center text-right">
+            <div class="d-flex justify-content-end align-items-center">
+                <ol class="breadcrumb">
+                    @foreach ($breadcrumbs as $breadcrumb)
+                        @if ($breadcrumb->url && !$loop->last)
+                            <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                        @else
+                            <li class=" breadcrumb-item active">{{ $breadcrumb->title }}</li>
+                        @endif
+                    @endforeach
+                </ol>
+            </div>
+        </div>
+    </div>
 @endif
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -64,9 +64,9 @@
     <!-- ============================================================== -->
     <!-- Left Sidebar - style you can find in sidebar.scss  -->
     <!-- ============================================================== -->
-    @include('backend.includes.sidebar-dynamic')
+@include('backend.includes.sidebar-dynamic')
 
-    <!-- ============================================================== -->
+<!-- ============================================================== -->
     <!-- End Left Sidebar - style you can find in sidebar.scss  -->
     <!-- ============================================================== -->
     <!-- ============================================================== -->
@@ -80,24 +80,12 @@
             <!-- ============================================================== -->
             <!-- Bread crumb and right sidebar toggle -->
             <!-- ============================================================== -->
-            <div class="row page-titles">
-                <div class="col-md-5 align-self-center">
-                    <h4 class="text-themecolor">Dashboard 1</h4>
-                </div>
-                <div class="col-md-7 align-self-center text-right">
-                    <div class="d-flex justify-content-end align-items-center">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard 1</li>
-                        </ol>
-                        <button type="button" class="btn btn-success d-none d-lg-block m-l-15"><i
-                                    class="fa fa-plus-circle"></i>
-                            Create New
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
+        @yield('page-header')
+        <!-- Breadcrumbs would render from routes/breadcrumb.php -->
+        @if(Breadcrumbs::exists())
+            {!! Breadcrumbs::render() !!}
+        @endif
+        <!-- ============================================================== -->
             <!-- End Bread crumb and right sidebar toggle -->
             <!-- ============================================================== -->
             <div class="card-group">
@@ -1715,91 +1703,6 @@
                     </div>
                 </div>
             </div>
-            <!-- ============================================================== -->
-            <!-- Right sidebar -->
-            <!-- ============================================================== -->
-            <!-- .right-sidebar -->
-            <div class="right-sidebar ps-container ps-theme-default" data-ps-id="b6ebefa8-f1c7-8bd0-def1-28e4694464e6">
-                <div class="slimscrollright">
-                    <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span>
-                    </div>
-                    <div class="r-panel-body">
-                        <ul id="themecolors" class="m-t-20">
-                            <li><b>With Light sidebar</b></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme">1</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme">4</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-purple" class="purple-theme">5</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-megna" class="megna-theme">6</a></li>
-                            <li class="d-block m-t-30"><b>With Dark sidebar</b></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-default-dark"
-                                   class="default-dark-theme">7</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-green-dark" class="green-dark-theme">8</a>
-                            </li>
-                            <li><a href="javascript:void(0)" data-skin="skin-red-dark" class="red-dark-theme">9</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-blue-dark" class="blue-dark-theme">10</a>
-                            </li>
-                            <li><a href="javascript:void(0)" data-skin="skin-purple-dark"
-                                   class="purple-dark-theme">11</a></li>
-                            <li><a href="javascript:void(0)" data-skin="skin-megna-dark"
-                                   class="megna-dark-theme working">12</a></li>
-                        </ul>
-                        <ul class="m-t-20 chatonline">
-                            <li><b>Chat option</b></li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/1.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Varun Dhavan <small
-                                                class="text-success">online</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/2.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Genelia Deshmukh <small
-                                                class="text-warning">Away</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/3.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Ritesh Deshmukh <small
-                                                class="text-danger">Busy</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/4.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Arijit Sinh <small
-                                                class="text-muted">Offline</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/5.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Govinda Star <small
-                                                class="text-success">online</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/6.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>John Abraham<small
-                                                class="text-success">online</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/7.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Hritik Roshan<small
-                                                class="text-success">online</small></span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)"><img src="images/8.jpg" alt="user-img"
-                                                                  class="img-circle"> <span>Pwandeep rajan <small
-                                                class="text-success">online</small></span></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
-                    <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-                </div>
-                <div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px;">
-                    <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
-            <!-- End Right sidebar -->
-            <!-- ============================================================== -->
         </div>
         <!-- ============================================================== -->
         <!-- End Container fluid  -->
@@ -1811,9 +1714,9 @@
     <!-- ============================================================== -->
     <!-- footer -->
     <!-- ============================================================== -->
-    @include('backend.includes.footer')
+@include('backend.includes.footer')
 
-    <!-- ============================================================== -->
+<!-- ============================================================== -->
     <!-- End footer -->
     <!-- ============================================================== -->
 </div>
