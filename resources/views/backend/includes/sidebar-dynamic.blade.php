f9ce8a6b7a8ecb7d281347a082f8d992694134cd
NGUYEN NGOC TUAN <VACNGTU001@VIETACORP.NET>
2020-08-19 16:04:24 +0700

    fix sidebar

--- resources/views/backend/includes/sidebar-dynamic.blade.php
+++ resources/views/backend/includes/sidebar-dynamic.blade.php
@@ -11,47 +11,14 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="icon-speedometer"></i><span class="hide-menu">Dashboard <span
-                                    class="badge badge-pill badge-cyan">4</span></span></a>
 
+                <li class="{{ active_class(Active::checkUriPattern('admin/dashboard')) }}">
+                    <a href="{{ route('admin.dashboard') }}">
+                        <i class="fa fa-dashboard"></i>
+                        <span>{{ trans('menus.backend.sidebar.dashboard') }}</span>
+                    </a>
                 </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-layout-grid2"></i><span class="hide-menu">Apps</span></a>
-
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark two-column" href="javascript:void(0)"
-                       aria-expanded="false"><i
-                                class="ti-palette"></i><span class="hide-menu">Ui</span></a>
-
-                </li>
-                <li class="nav-small-cap"></li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-layout-media-right-alt"></i><span class="hide-menu">Forms</span></a>
-
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-layout-accordion-merged"></i><span class="hide-menu">Tables</span></a>
-
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-settings"></i><span class="hide-menu">Widgets</span></a>
-
-                </li>
-                <li class="nav-small-cap"></li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-files"></i><span class="hide-menu">Pages <span
-                                    class="badge badge-pill badge-info">25</span></span></a>
-
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-light-bulb"></i><span class="hide-menu">Extras</span></a>
-
-                </li>
-                <li><a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i
-                                class="ti-align-left"></i><span class="hide-menu">Multi dd</span></a>
-
-                </li>
+                {{ renderMenuItems(getMenuItems()) }}
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
